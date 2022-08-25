b421cb3cfe0fc78a6b007577717f8f9402315537
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-25 12:53:21 +0800

    pod mainline

--- src/clj/bbssh/impl/pod.clj
+++ src/clj/bbssh/impl/pod.clj
@@ -1,12 +1,14 @@
 (ns bbssh.impl.pod
   (:refer-clojure :exclude [read-string read])
-  (:require [bencode.core :refer [read-bencode write-bencode]])
+  (:require [bencode.core :refer [read-bencode write-bencode]]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io])
   (:import [java.io PushbackInputStream]
            [java.net ServerSocket]))
 
 (set! *warn-on-reflection* true)
 
-(def debug? false)
+(def debug? true)
 (def debug-file "/tmp/bbssh-pod-debug.txt")
 
 (defn read-string [^"[B" v]
@@ -29,3 +31,80 @@
 (defn safe-read [d]
   (when d
     (read-string d)))
+
+(def lookup {})
+
+(defn main []
+  (let [server (ServerSocket. 0)
+        port (.getLocalPort server)
+        pid (.pid (java.lang.ProcessHandle/current))
+        port-file (io/file (str ".babashka-pod-" pid ".port"))
+        _ (.addShutdownHook (Runtime/getRuntime)
+                            (Thread. (fn []
+                                       (.delete port-file))))
+        _ (spit port-file
+                (str port "\n"))
+        socket (.accept server)
+        in (PushbackInputStream. (.getInputStream socket))
+        out (.getOutputStream socket)
+        ]
+    (try
+      (loop []
+        (let [{:strs [id op var args ns]} (read in)
+              id-decoded (safe-read id)
+              op-decoded (safe-read op)
+              var-decoded (safe-read var)
+              args-decoded (safe-read args)
+              ns-decoded (safe-read ns)]
+          (debug 'id id-decoded
+                 'op op-decoded
+                 'var var-decoded
+                 'args args-decoded
+                 'ns ns-decoded)
+          (case op-decoded
+            "describe"
+            (do
+              (write out
+                     {"port" port
+                      "format" "edn"
+                      "namespaces"
+                      []
+                      "id" (read-string id)})
+              (recur))
+            "load-ns"
+            (do
+              ;;(prn "load-ns")
+              (recur)
+              )
+            "invoke"
+            (do
+              (try
+                (let [var (-> var
+                              read-string
+                              symbol)
+                      args args-decoded]
+                  (debug 'invoke var args)
+                  (let [args (edn/read-string args)]
+                    (if-let [f (lookup var)]
+                      (let [value (pr-str (apply f args))
+                            reply {"value" value
+                                   "id" id
+                                   "status" ["done"]}]
+                        (debug 'reply reply)
+                        (write out reply))
+                      (throw (ex-info (str "Var not found: " var) {})))))
+                (catch Throwable e
+                  (binding [*out* *err*]
+                    (println e))
+                  (let [reply {"ex-message" (.getMessage e)
+                               "ex-data" (pr-str
+                                          (assoc (ex-data e)
+                                                 :type (class e)))
+                               "id" id
+                               "status" ["done" "error"]}]
+                    (write out reply))))
+              (recur))
+            (do
+              (write out {"err" (str "unknown op:" (name op))})
+              (recur)))))
+      (catch java.io.EOFException _ nil))))

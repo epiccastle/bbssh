9f8a0ada841c55ad6594a7fbacd4ab9ef101a498
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-29 12:10:14 +0800

    no imprts

--- src/clj/pod/epiccastle/bbssh/core.clj
+++ src/clj/pod/epiccastle/bbssh/core.clj
@@ -1,4 +1,1 @@
-(ns pod.epiccastle.bbssh.core
-  (:require [pod.epiccastle.bbssh.impl.core :as core])
-  (:import [java.lang.ref WeakReference ReferenceQueue Cleaner]
-           [java.util WeakHashMap Collections]))
+(ns pod.epiccastle.bbssh.core)

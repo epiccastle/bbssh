11b339c13f4c51be3c28630ba1fd34922ab199be
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-29 11:11:00 +0800

    basic bb test code

--- test/bb.clj
+++ test/bb.clj
@@ -1,7 +1,8 @@
 (ns my-project.core
   (:require [babashka.pods :as pods]
             [babashka.process :as process]
-            [clojure.string :as string]))
+            [clojure.string :as string])
+  (:import [java.lang.ref WeakReference]))
 
 (def class-path
   (-> "clj -Spath -m bbssh.core"
@@ -24,3 +25,45 @@
 #_(pods/load-pod "./bbssh" {:transport :socket})
 
 (println "loaded")
+
+(require '[pod.epiccastle.bbssh.agent :as agent]
+         '[pod.epiccastle.bbssh.session :as session]
+         '[pod.epiccastle.bbssh.impl.cleaner :as cleaner])
+
+
+(defn gc []
+  (println "gc")
+  (System/gc)
+  (System/runFinalization)
+  (println "gc done")
+  )
+
+(defn force-gc []
+  (println "force-gc")
+  (let [t (atom (Object.))
+        wr (WeakReference. @t)]
+    (reset! t nil)
+    (while (.get wr)
+      (System/gc)
+      (System/runFinalization)))
+  (println "force-gc done"))
+
+(defn foo []
+  (prn 1)
+  (let [a (agent/new)]
+    (prn 2)
+    (println "new returned:" a)
+    (force-gc)
+    (force-gc)
+    (let [s (agent/get-session a "crispin" "localhost" 22)]
+      (println "get-session returned:" s)
+      (force-gc)
+      (prn (cleaner/get-references))
+      )
+    ))
+
+(foo)
+(force-gc)
+(force-gc)
+(Thread/sleep 1000)
+(prn (cleaner/get-references))

479b6647d82508ea63a0e35e2d6818e270d8b45e
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-30 12:06:03 +0800

    base64 encode/decode utils

--- src/clj/bbssh/impl/utils.clj
+++ src/clj/bbssh/impl/utils.clj
@@ -1,5 +1,6 @@
 (ns bbssh.impl.utils
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string])
+  (:import [java.util Base64 Base64$Encoder Base64$Decoder]))
 
 (set! *warn-on-reflection* true)
 
@@ -8,3 +9,18 @@
 
 (defn string-to-byte-array [^String s]
   (byte-array (map int s)))
+
+(defn boolean-to-yes-no [val]
+  (if (boolean? val)
+    (if val "yes" "no")
+    val))
+
+(def base64-encoder (Base64/getEncoder))
+
+(defn encode-base64 [^bytes to-encode]
+  (.encodeToString ^Base64$Encoder base64-encoder to-encode))
+
+(def base64-decoder (Base64/getDecoder))
+
+(defn decode-base64 [^String to-decode]
+  (.decode ^Base64$Decoder base64-decoder to-decode))

cd67f4e7d1fd184b0aa84edc723b5fe81d150ac6
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-30 12:03:38 +0800

    docstrings

--- src/clj/pod/epiccastle/bbssh/agent.clj
+++ src/clj/pod/epiccastle/bbssh/agent.clj
@@ -2,9 +2,17 @@
   (:require [pod.epiccastle.bbssh.impl.agent :as agent]
             [pod.epiccastle.bbssh.cleaner :as cleaner]))
 
-(defn new []
+(defn new
+  "Make a new JSch agent. A JSch agent is not an \"ssh agent\".
+  It is simply the base java class that holds and controls the
+  sessions."
+  []
   (cleaner/register (agent/new)))
 
-(defn get-session [agent username host port]
+(defn get-session
+  "From the JSch agent, construct a new JSch session. Does not
+  start the ssh connection.
+  "
+  [agent username host port]
   (cleaner/register
    (agent/get-session (cleaner/split-key agent) username host port)))

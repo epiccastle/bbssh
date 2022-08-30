36d920849882f4f8203556ed60a4d06e0f81c075
Crispin Wellington <retrogradeorbit@gmail.com>
2022-08-30 12:04:06 +0800

    more JSch Session support

--- src/clj/pod/epiccastle/bbssh/impl/session.clj
+++ src/clj/pod/epiccastle/bbssh/impl/session.clj
@@ -1,6 +1,7 @@
 (ns pod.epiccastle.bbssh.impl.session
-  (:require [bbssh.impl.references :as references])
-  (:import [com.jcraft.jsch JSch Session])
+  (:require [bbssh.impl.references :as references]
+            [bbssh.impl.utils :as utils])
+  (:import [com.jcraft.jsch JSch Session UserInfo])
   )
 
 ;; pod.epiccastle.bbssh.impl.* are invoked on pod side.
@@ -19,3 +20,102 @@
   (.setPassword
    ^Session (references/get-instance session)
    ^String password))
+
+(defn set-user-info [session user-info]
+  (.setUserInfo
+   ^Session (references/get-instance session)
+   ^UserInfo (references/get-instance user-info)))
+
+(defn connect [session & [timeout]]
+  (if timeout
+    (.connect
+     ^Session (references/get-instance session)
+     timeout)
+    (.connect
+     ^Session (references/get-instance session))))
+
+(defn disconnect [session]
+  (.disconnect
+   ^Session (references/get-instance session)))
+
+(defn set-port-forwarding-local
+  [session
+   {:keys [bind-address
+           local-port
+           remote-host
+           remote-port
+           connect-timeout]
+    :or {bind-address "127.0.0.1"}}]
+  (.setPortForwardingL
+   ^Session (references/get-instance session)
+   ^String bind-address
+   ^int local-port
+   ^String remote-host
+   ^int remote-port
+   nil
+   ^int connect-timeout)
+
+  )
+
+(defn delete-port-forwarding-local
+  [session
+   {:keys [bind-address
+           local-port
+           remote-host
+           remote-port
+           connect-timeout]
+    :or {bind-address "127.0.0.1"}}]
+  (.setPortForwardingL
+   ^Session (references/get-instance session)
+   ^String bind-address
+   ^int local-port
+   ^String remote-host
+   ^int remote-port
+   nil
+   ^int connect-timeout)
+
+  )
+
+(defn get-port-forwarding-local
+  [session]
+  (.getPortForwardingL
+   ^Session (references/get-instance session)))
+
+(defn set-host
+  [session host]
+  (.setHost
+   ^Session (references/get-instance session)
+   ^String host))
+
+(defn set-port
+  [session port]
+  (.setHost
+   ^Session (references/get-instance session)
+   ^int port))
+
+(defn set-config
+  [session key value]
+  (.setConfig
+   ^Session (references/get-instance session)
+   ^String (utils/to-camel-case (name key))
+   ^String (utils/boolean-to-yes-no value)))
+
+(defn get-config
+  [session key]
+  (.getConfig
+   ^Session (references/get-instance session)
+   ^String (utils/to-camel-case (name key))))
+
+(defn connected?
+  [session]
+  (.isConnected
+   ^Session (references/get-instance session)))
+
+(defn open-channel
+  [session type]
+  (references/add-instance
+   (.openChannel
+    ^Session (references/get-instance session)
+    ^String type)
+   ;;"pod.epiccastle.bbssh.channel-exec" "channel-exec"
+   ))
--- src/clj/pod/epiccastle/bbssh/session.clj
+++ src/clj/pod/epiccastle/bbssh/session.clj
@@ -2,11 +2,110 @@
   (:require [pod.epiccastle.bbssh.impl.session :as session]
             [pod.epiccastle.bbssh.cleaner :as cleaner]))
 
-(defn new [agent username host port]
-  (cleaner/register
-   (session/new agent username host port)))
-
-(defn set-password [session password]
+(defn set-password
+  "Set the password the session will use to authenticate to
+  `password`"
+  [session password]
   (session/set-password
    (cleaner/split-key session)
    password))
+
+(defn set-user-info
+  "Set the user-info structure the session will use to gather
+  keys and associated information from to `user-info`"
+  [session user-info]
+  (session/set-user-info
+   (cleaner/split-key session)
+   (cleaner/split-key user-info)))
+
+(defn connect
+  "Initiate the ssh connection with an optional `timeout`
+  (in milliseconds)."
+  [session & [timeout]]
+  (session/connect
+   (cleaner/split-key session)
+   timeout))
+
+(defn disconnect
+  "Disconnect the ssh connection"
+  [session]
+  (session/disconnect
+   (cleaner/split-key session)))
+
+(defn set-port-forwarding-local
+  "Register the local port to forward all connection to the remote
+  side, where they will connect to a remote host on a port.
+
+  `options` is a hashmap for the following form
+
+  ```clj
+  {
+    :bind-address \"127.0.0.1\"              ;; the local interface to bind to. Use \"*\" or \"0.0.0.0\" for all interfaces.
+    :local-port 2200                         ;; the local port to listen on
+    :remote-host \"jump-target.domain.com\"  ;; the remote host to forward the connection to on the remote side
+    :remote-port 22                          ;; the remote port to forward to
+    :connect-timeout 30000                   ;; how long to try to connect for
+  }
+  ```
+  "
+  [session options]
+  (session/set-port-forwarding-local
+   (cleaner/split-key session)
+   options))
+
+(defn delete-port-forwarding-local
+  "Cancels the specified local port forwarding"
+  [session options]
+  (session/delete-port-forwarding-local
+   (cleaner/split-key session)
+   options))
+
+(defn get-port-forwarding-local
+  "return a list of all the local port forwards. List elements
+  are of the form \"local-port:host:host-port\"."
+  [session]
+  (session/get-port-forwarding-local
+   (cleaner/split-key session)))
+
+(defn set-host
+  "Set the host to connect to"
+  [session host]
+  (session/set-host
+   (cleaner/split-key session)
+   host))
+
+(defn set-port
+  "Set the port to connect to"
+  [session port]
+  (session/set-port
+   (cleaner/split-key session)
+   port))
+
+(defn set-config
+  "Set the config setting `key` to `value`"
+  [session key value]
+  (session/set-config
+   (cleaner/split-key session)
+   key
+   value))
+
+(defn get-config
+  "Get the current config setting `key`"
+  [session key]
+  (session/get-config
+   (cleaner/split-key session)
+   key))
+
+(defn connected?
+  "return true if session is currently connected"
+  [session]
+  (session/connected?
+   (cleaner/split-key session)))
+
+(defn open-channel
+  "open a channel on the session and return it"
+  [session type]
+  (cleaner/register
+   (session/open-channel
+    (cleaner/split-key session)
+    type)))

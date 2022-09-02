51337dc37775a20fc7124f4da92c3c3b1f18354f
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-02 22:35:59 +0800

    test garbage collection of user-info

--- test/bb.clj
+++ test/bb.clj
@@ -29,33 +29,46 @@
  '[pod.epiccastle.bbssh.user-info :as user-info]
  '[pod.epiccastle.bbssh.session :as session]
  '[pod.epiccastle.bbssh.agent :as agent]
+ '[pod.epiccastle.bbssh.impl.cleaner :as cleaner]
  )
 
-(let [ui (user-info/new
-          {:get-password (fn []
-                           (prn :get-password)
-                           "incorrect-password")
-           :prompt-yes-no (fn [s]
-                            (prn :prompt-yes-no s)
-                            true)
-           :get-passphrase (fn []
-                             (prn :get-passphrase)
-                             "passphrase")
-           :prompt-passphrase (fn [s]
-                                (prn :prompt-passphrase s)
-                                true)
-           :prompt-password (fn [s]
-                              (prn :prompt-password s)
+(defn foo []
+  (let [ui (user-info/new
+            {:get-password (fn []
+                             (prn :get-password)
+                             "incorrect-password")
+             :prompt-yes-no (fn [s]
+                              (prn :prompt-yes-no s)
                               true)
-           :show-message (fn [s]
-                           (prn :show-message s))})
+             :get-passphrase (fn []
+                               (prn :get-passphrase)
+                               "passphrase")
+             :prompt-passphrase (fn [s]
+                                  (prn :prompt-passphrase s)
+                                  true)
+             :prompt-password (fn [s]
+                                (prn :prompt-password s)
+                                true)
+             :show-message (fn [s]
+                             (prn :show-message s))})
 
-      agent (agent/new)
-      session (agent/get-session agent
-                                 (System/getenv "USER")
-                                 "localhost"
-                                 22)
-      ]
-  (session/set-config session :strict-host-key-checking false)
-  (session/set-user-info session ui)
-  (session/connect session))
+        agent (agent/new)
+        session (agent/get-session agent
+                                   (System/getenv "USER")
+                                   "localhost"
+                                   22)
+        ]
+    (session/set-config session :strict-host-key-checking false)
+    (session/set-user-info session ui)
+    (try (session/connect session)
+         (catch Exception e))
+
+    )
+  )
+
+(foo)
+(prn "running gc...")
+(System/gc)
+(Thread/sleep 1000)
+(prn "all cleaned?" (empty? (cleaner/get-references)))
+(Thread/sleep 1000)

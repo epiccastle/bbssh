b3a36f4171a3706a673c90b9bfd2db47f21fb777
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-02 15:39:13 +0800

    user-info structure

---
+++ src/bb/pod/epiccastle/bbssh/user_info.clj
@@ -0,0 +1,38 @@
+(ns pod.epiccastle.bbssh.user-info
+  (:require [pod.epiccastle.bbssh.impl.user-info :as user-info]
+            [pod.epiccastle.bbssh.cleaner :as cleaner]))
+
+(defn new [callbacks]
+  (let [p (promise)]
+    (babashka.pods/invoke
+     "pod.epiccastle.bbssh"
+     'pod.epiccastle.bbssh.impl.user-info/new
+     []
+     {:handlers
+      {:success
+       (fn [[method value]]
+         (case method
+
+           ;; the returned object
+           :result
+           (deliver
+            p
+            (cleaner/register value))
+
+           ;; a callback message
+           :method
+           (let [{:keys [id method args]} value
+                 f (callbacks method (fn [&args]))]
+             (let [result (apply f args)]
+               (future
+                 ;; without the future here I get into a deadlock
+                 ;; within bb/sci. The babashka.pods.impl/processor
+                 ;; function gets blocked forever by the success
+                 ;; callback
+                 ;; https://github.com/babashka/pods/blob/eb0b01c0a69cf7ef24b0277d4449a157253a3037/src/babashka/pods/impl.clj#L231
+                 ;; and then it can't process any more async
+                 ;; responses.
+                (user-info/sync-result id result))))))
+       :error (fn [err]
+                (prn 'error err))}})
+    @p))

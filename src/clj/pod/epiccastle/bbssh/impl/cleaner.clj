464c617b2ef42d6b14ffc6e99a2b017b6f4335e8
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-02 22:29:35 +0800

    reference deletion callbacks

--- src/clj/pod/epiccastle/bbssh/impl/cleaner.clj
+++ src/clj/pod/epiccastle/bbssh/impl/cleaner.clj
@@ -1,8 +1,32 @@
 (ns pod.epiccastle.bbssh.impl.cleaner
-  (:require [bbssh.impl.references :as references]))
+  (:require [bbssh.impl.references :as references]
+            [clojure.set :as set]))
+
+(def delete-fns (atom {}))
+
+(defonce _watch
+  (add-watch delete-fns :deleter
+             (fn [_ _ o n]
+               ;; when a key is dissoced
+               (doseq [k (set/difference
+                          (into #{} (keys o))
+                          (into #{} (keys n)))]
+                 ;; trigger the final deletion callbacks
+                 (let [cbs (o k)]
+                   (doseq [cb cbs]
+                     (cb)))))))
+
+(defn register-delete-fn [key delete-fn]
+  (swap! delete-fns
+         update key
+         (fn [v]
+           (if v
+             (conj v delete-fn)
+             [delete-fn]))))
 
 (defn del-reference [key]
   (references/delete-key key)
+  (swap! delete-fns dissoc key) ;; removing the key triggers deletion callbacks
   nil)
 
 (defn get-references []

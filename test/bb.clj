b38ed1db1ef15e6388ec0631f2d58b740a5e51ae
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-02 15:46:05 +0800

    test user info

--- test/bb.clj
+++ test/bb.clj
@@ -26,75 +26,37 @@
 
 (println "loaded")
 
-(require '[pod.epiccastle.bbssh.agent :as agent]
-         '[pod.epiccastle.bbssh.session :as session]
-         '[pod.epiccastle.bbssh.channel-exec :as channel-exec]
-         '[pod.epiccastle.bbssh.input-stream :as input-stream]
-         '[pod.epiccastle.bbssh.output-stream :as output-stream]
-         '[pod.epiccastle.bbssh.impl.cleaner :as cleaner])
+(require
+ '[pod.epiccastle.bbssh.user-info :as user-info]
+ '[pod.epiccastle.bbssh.session :as session]
+ '[pod.epiccastle.bbssh.agent :as agent]
+ )
 
+(let [ui (user-info/new
+          {:get-password (fn []
+                           (prn :get-password)
+                           "incorrect-password")
+           :prompt-yes-no (fn [s]
+                            (prn :prompt-yes-no s)
+                            true)
+           :get-passphrase (fn []
+                             (prn :get-passphrase)
+                             "passphrase")
+           :prompt-passphrase (fn [s]
+                                (prn :prompt-passphrase s)
+                                true)
+           :prompt-password (fn [s]
+                              (prn :prompt-password s)
+                              true)
+           :show-message (fn [s]
+                           (prn :show-message s))})
 
-(defn gc []
-  (println "gc")
-  (System/gc)
-  (System/runFinalization)
-  (println "gc done")
-  )
-
-(defn force-gc []
-  (println "force-gc")
-  (let [t (atom (Object.))
-        wr (WeakReference. @t)]
-    (reset! t nil)
-    (while (.get wr)
-      (System/gc)
-      (System/runFinalization)))
-  (println "force-gc done"))
-
-(defn streams-for-out []
-  (let [os (output-stream/new)
-        is (input-stream/new os 1024)]
-    [os is]))
-
-(defn streams-for-in []
-  (let [is (input-stream/new 1024)
-        os (output-stream/new is)]
-    [is os]))
-
-(defn foo []
-  (let [a (agent/new)]
-    (println "new returned:" a)
-    (let [s (agent/get-session a (System/getenv "USER") "localhost" 22)]
-      (println "get-session returned:" s)
-      (prn (cleaner/get-references))
-      (println "password:")
-      (session/set-password s (read-line))
-      (session/set-config s :strict-host-key-checking false)
-      (session/connect s)
-      (prn 'connected? (session/connected? s))
-      (let [c (session/open-channel s "exec")
-
-            is (input-stream/new)
-
-            [out-stream out-in] (streams-for-out)
-            [err-stream err-in] (streams-for-out)
-            ]
-        (channel-exec/set-command c "id")
-        (channel-exec/set-input-stream c is false)
-        (input-stream/close is)
-
-        (channel-exec/set-output-stream c out-stream)
-        (channel-exec/set-error-stream c err-stream)
-
-        (channel-exec/connect c)
-        (prn "reading...")
-        (let [buff (byte-array 1024)
-              num (input-stream/read out-in buff 0 1024)]
-          (prn num)
-          (prn (String. (java.util.Arrays/copyOfRange buff 0 num) "UTF-8")))))))
-
-(foo)
-
-(force-gc)
-(Thread/sleep 1000)
-(prn (cleaner/get-references))
+      agent (agent/new)
+      session (agent/get-session agent
+                                 (System/getenv "USER")
+                                 "localhost"
+                                 22)
+      ]
+  (session/set-config session :strict-host-key-checking false)
+  (session/set-user-info session ui)
+  (session/connect session))

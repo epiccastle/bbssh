f006a015293ceb7e42d5c1461a7646fd4d026708
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-06 15:51:07 +0800

    future wrap the apply too

--- src/bb/pod/epiccastle/bbssh/user_info.clj
+++ src/bb/pod/epiccastle/bbssh/user_info.clj
@@ -22,16 +22,15 @@
            :method
            (let [{:keys [id method args]} value
                  f (callbacks method (fn [&args]))]
-             (let [result (apply f args)]
-               (future
-                 ;; without the future here I get into a deadlock
-                 ;; within bb/sci. The babashka.pods.impl/processor
-                 ;; function gets blocked forever by the success
-                 ;; callback
-                 ;; https://github.com/babashka/pods/blob/eb0b01c0a69cf7ef24b0277d4449a157253a3037/src/babashka/pods/impl.clj#L231
-                 ;; and then it can't process any more async
-                 ;; responses.
-                 (callbacks/return-result id result))))
+             ;; without the future here I get into a deadlock within
+             ;; bb/sci. The babashka.pods.impl/processor
+             ;; function gets blocked forever by the success
+             ;; callback
+             ;; https://github.com/babashka/pods/blob/eb0b01c0a69cf7ef24b0277d4449a157253a3037/src/babashka/pods/impl.clj#L231
+             ;; and then it can't process any more async
+             ;; responses.
+             (future
+               (callbacks/return-result id (apply f args))))
 
            ;; final message apon deletion triggers this response
            :done

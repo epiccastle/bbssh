f96df488026c48ed02bcd7f7cd33417879b5f29e
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-06 22:19:27 +0800

    use fingerprints from key struct

--- test/bb_test/test_key_pair.clj
+++ test/bb_test/test_key_pair.clj
@@ -13,29 +13,24 @@
             [clojure.test :refer [is deftest]]
             [clojure.string :as string]))
 
-(defn create-key-pair [agent key-id]
-  (spit "/tmp/bbssh-test-key" (get-in keys/keys [key-id :private]))
-  (spit "/tmp/bbssh-test-key.pub" (get-in keys/keys [key-id :public]))
-  (key-pair/load agent "/tmp/bbssh-test-key" "/tmp/bbssh-test-key.pub"))
-
 (deftest identity-repository
   (docker/cleanup)
   (docker/build {:root-password "root-access-please"})
   (docker/start {:ssh-port 9876})
 
   (let [agent (agent/new)
-        kp (create-key-pair agent :rsa-nopassphrase)]
+        kp (keys/create-key-pair agent :rsa-nopassphrase)]
     (is (= (key-pair/get-finger-print kp)
-           "e6:a0:ce:eb:00:0c:6d:a3:8f:b6:cb:c0:ea:47:74:74"))
+             (get-in keys/keys [:rsa-nopassphrase :fingerprint])))
     (is (= (key-pair/get-key-size kp)
            1024))
     (is (= (seq (key-pair/get-public-key-blob kp))
            (get-in keys/keys [:rsa-nopassphrase :public-blob])))
     (is (not (key-pair/is-encrypted kp)))
 
-    (let [kp (create-key-pair agent :rsa-passphrase)]
+    (let [kp (keys/create-key-pair agent :rsa-passphrase)]
       (is (= (key-pair/get-finger-print kp)
-             "58:a5:65:1c:8c:12:c9:6e:67:aa:70:e6:11:e3:c3:10"))
+             (get-in keys/keys [:rsa-passphrase :fingerprint])))
       (is (= (key-pair/get-key-size kp)
              1024))
       (is (= (seq (key-pair/get-public-key-blob kp))

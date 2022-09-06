b291b294dfa847604491cc4fae41789d40528210
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-06 22:36:55 +0800

    use the utils/new-invoker function

--- src/bb/pod/epiccastle/bbssh/identity_repository.clj
+++ src/bb/pod/epiccastle/bbssh/identity_repository.clj
@@ -1,7 +1,5 @@
 (ns pod.epiccastle.bbssh.identity-repository
-  (:require [pod.epiccastle.bbssh.impl.identity-repository :as identity-repository]
-            [pod.epiccastle.bbssh.impl.callbacks :as callbacks]
-            [pod.epiccastle.bbssh.cleaner :as cleaner]
+  (:require [pod.epiccastle.bbssh.cleaner :as cleaner]
             [pod.epiccastle.bbssh.utils :as utils]))
 
 (defn- preprocess-args [method args]
@@ -24,41 +22,9 @@
     result))
 
 (defn new [callbacks]
-  (let [p (promise)]
-    (babashka.pods/invoke
-     "pod.epiccastle.bbssh"
-     'pod.epiccastle.bbssh.impl.identity-repository/new
-     []
-     {:handlers
-      {:success
-       (fn [[method value]]
-         (case method
-
-           ;; the returned object
-           :result
-           (deliver p value)
-
-           ;; a callback message
-           :method
-           (let [{:keys [id method args]} value
-                 f (callbacks method (fn [& args]))]
-             ;; without the future here I get into a deadlock
-             ;; within bb/sci. The babashka.pods.impl/processor
-             ;; function gets blocked forever by the success
-             ;; callback
-             ;; https://github.com/babashka/pods/blob/eb0b01c0a69cf7ef24b0277d4449a157253a3037/src/babashka/pods/impl.clj#L231
-             ;; and then it can't process any more async
-             ;; responses.
-             (future
-               (->> args
-                    (preprocess-args method)
-                    (apply f)
-                    (postprocess-returns method)
-                    (callbacks/return-result id))))
-
-           ;; final message apon deletion triggers this response
-           :done
-           nil))
-       :error (fn [err]
-                (prn 'error err))}})
-    (cleaner/register @p)))
+  (utils/new-invoker
+   {:call-sym 'pod.epiccastle.bbssh.impl.identity-repository/new
+    :args []
+    :callbacks callbacks
+    :preprocess-args-fn preprocess-args
+    :postprocess-returns-fn postprocess-returns}))
--- src/bb/pod/epiccastle/bbssh/user_info.clj
+++ src/bb/pod/epiccastle/bbssh/user_info.clj
@@ -1,40 +1,8 @@
 (ns pod.epiccastle.bbssh.user-info
-  (:require [pod.epiccastle.bbssh.impl.user-info :as user-info]
-            [pod.epiccastle.bbssh.impl.callbacks :as callbacks]
-            [pod.epiccastle.bbssh.cleaner :as cleaner]))
+  (:require [pod.epiccastle.bbssh.utils :as utils]))
 
 (defn new [callbacks]
-  (let [p (promise)]
-    (babashka.pods/invoke
-     "pod.epiccastle.bbssh"
-     'pod.epiccastle.bbssh.impl.user-info/new
-     []
-     {:handlers
-      {:success
-       (fn [[method value]]
-         (case method
-
-           ;; the returned object
-           :result
-           (deliver p value)
-
-           ;; a callback message
-           :method
-           (let [{:keys [id method args]} value
-                 f (callbacks method (fn [&args]))]
-             ;; without the future here I get into a deadlock within
-             ;; bb/sci. The babashka.pods.impl/processor
-             ;; function gets blocked forever by the success
-             ;; callback
-             ;; https://github.com/babashka/pods/blob/eb0b01c0a69cf7ef24b0277d4449a157253a3037/src/babashka/pods/impl.clj#L231
-             ;; and then it can't process any more async
-             ;; responses.
-             (future
-               (callbacks/return-result id (apply f args))))
-
-           ;; final message apon deletion triggers this response
-           :done
-           nil))
-       :error (fn [err]
-                (prn 'error err))}})
-    (cleaner/register @p)))
+  (utils/new-invoker
+   {:call-sym 'pod.epiccastle.bbssh.impl.user-info/new
+    :args []
+    :callbacks callbacks}))

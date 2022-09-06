0334a8fe29f5028b2ca01c455f97977f1937e62e
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-06 15:44:57 +0800

    identity namespace

---
+++ src/clj/pod/epiccastle/bbssh/impl/identity.clj
@@ -0,0 +1,47 @@
+(ns pod.epiccastle.bbssh.impl.identity
+  (:require [bbssh.impl.references :as references]
+            [bbssh.impl.utils :as utils]
+            [pod.epiccastle.bbssh.impl.callbacks :as callbacks]
+            [pod.epiccastle.bbssh.impl.cleaner :as cleaner])
+  (:import [com.jcraft.jsch Identity]))
+
+;; pod.epiccastle.bbssh.impl.* are invoked on pod side.
+
+(set! *warn-on-reflection* true)
+
+(defn ^:async new [reply-fn]
+  (let [result
+        (references/add-instance
+         (proxy [Identity] []
+           (setPassphrase [^bytes passphrase]
+             (callbacks/call-method
+              reply-fn :set-passphrase
+              [(some-> passphrase utils/encode-base64)]))
+           (getPublicKeyBlob []
+             (utils/decode-base64
+              (callbacks/call-method reply-fn :get-public-key-blob [])))
+           (getSignature
+             ([^bytes data]
+              (utils/decode-base64
+               (callbacks/call-method
+                reply-fn :get-signature
+                [(some-> data utils/encode-base64)])))
+             ([^bytes data ^String alg]
+              (utils/decode-base64
+               (callbacks/call-method
+                reply-fn :get-signature
+                [(some-> data utils/encode-base64) alg]))))
+           (decrypt []
+             (callbacks/call-method reply-fn :decrypt []))
+           (getAlgName []
+             (callbacks/call-method reply-fn :get-alg-name []))
+           (getName []
+             (callbacks/call-method reply-fn :get-name []))
+           (isEncrypted []
+             (callbacks/call-method reply-fn :is-encrypted []))
+           (clear []
+             (callbacks/call-method reply-fn :clear []))
+           ))]
+    (cleaner/register-delete-fn result #(reply-fn [:done] ["done"]))
+    (reply-fn [:result result])
+    nil))

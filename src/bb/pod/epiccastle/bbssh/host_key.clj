c71933db8f71da650ba6b7c985671ff3e66f0028
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-10 23:15:10 +0800

    implement host-key methods

--- src/bb/pod/epiccastle/bbssh/host_key.clj
+++ src/bb/pod/epiccastle/bbssh/host_key.clj
@@ -1,15 +1,52 @@
 (ns pod.epiccastle.bbssh.host-key
   (:require [pod.epiccastle.bbssh.impl.host-key :as host-key]
+            [pod.epiccastle.bbssh.cleaner :as cleaner]
             [pod.epiccastle.bbssh.utils :as utils])
  )
 
 (defn new
   "Create a new host-key."
   ([^String host ^bytes key]
-   (host-key/new host (utils/encode-base64 key)))
+   (cleaner/register
+    (host-key/new host (utils/encode-base64 key))))
   ([^String host ^clojure.lang.Keyword type ^bytes key]
-   (host-key/new host type (utils/encode-base64 key)))
+   (cleaner/register
+    (host-key/new host type (utils/encode-base64 key))))
   ([^String host ^clojure.lang.Keyword type ^bytes key ^String comment]
-   (host-key/new host type (utils/encode-base64 key) comment))
+   (cleaner/register
+    (host-key/new host type (utils/encode-base64 key) comment)))
   ([^String marker ^String host ^clojure.lang.Keyword type ^bytes key ^String comment]
-   (host-key/new marker host type (utils/encode-base64 key) comment)))
+   (cleaner/register
+    (host-key/new marker host type (utils/encode-base64 key) comment))))
+
+(defn get-host
+  [host-key]
+  (host-key/get-host
+   (cleaner/split-key host-key)))
+
+(defn get-type
+  [host-key]
+  (host-key/get-type
+   (cleaner/split-key host-key)))
+
+(defn get-key
+  [host-key]
+  (host-key/get-key
+   (cleaner/split-key host-key)))
+
+(defn get-finger-print
+  [host-key agent]
+  (host-key/get-finger-print
+   (cleaner/split-key host-key)
+   (cleaner/split-key agent)
+   ))
+
+(defn get-comment
+  [host-key]
+  (host-key/get-comment
+   (cleaner/split-key host-key)))
+
+(defn get-marker
+  [host-key]
+  (host-key/get-marker
+   (cleaner/split-key host-key)))
--- src/clj/pod/epiccastle/bbssh/impl/host_key.clj
+++ src/clj/pod/epiccastle/bbssh/impl/host_key.clj
@@ -1,7 +1,7 @@
 (ns pod.epiccastle.bbssh.impl.host-key
   (:require [bbssh.impl.references :as references]
             [bbssh.impl.utils :as utils])
-  (:import [com.jcraft.jsch HostKey]))
+  (:import [com.jcraft.jsch HostKey JSch]))
 
 (def types
   {:unknown HostKey/UNKNOWN
@@ -29,3 +29,34 @@
    (references/add-instance
     (HostKey. marker host (types type) (utils/decode-base64 key) comment))
    ))
+
+(defn get-host
+  [host-key]
+  (.getHost
+   ^HostKey (references/get-instance host-key)))
+
+(defn get-type
+  [host-key]
+  (.getType
+   ^HostKey (references/get-instance host-key)))
+
+(defn get-key
+  [host-key]
+  (.getKey
+   ^HostKey (references/get-instance host-key)))
+
+(defn get-finger-print
+  [host-key agent]
+  (.getFingerPrint
+   ^HostKey (references/get-instance host-key)
+   ^JSch (references/get-instance agent)))
+
+(defn get-comment
+  [host-key]
+  (.getComment
+   ^HostKey (references/get-instance host-key)))
+
+(defn get-marker
+  [host-key]
+  (.getMarker
+   ^HostKey (references/get-instance host-key)))

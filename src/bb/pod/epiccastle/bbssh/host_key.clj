591949abb5973b3b6fe3f88dbb9a7a3beb805c73
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-14 15:44:46 +0800

    host-key docs

--- src/bb/pod/epiccastle/bbssh/host_key.clj
+++ src/bb/pod/epiccastle/bbssh/host_key.clj
@@ -5,7 +5,17 @@
  )
 
 (defn new
-  "Create a new host-key."
+  "Create a new host-key with some subset of:
+
+  - `host`: A string of the hostname.
+  - `key`: A byte array of the public key.
+  - `type`: One of `:unknown`, `:guess`, `:sshdss`, `:sshrsa`,
+          `:ecdsa256`, `:ecdsa384`, `:ecdsa521`, `:ed25519` or `:ed448`.
+          If uknown value passed, defaults to `:guess` which tries to
+          guess the key type.
+  - `comment`: A string comment for the key.
+  - `marker`: A string marker for the key.
+  "
   ([^String host ^bytes key]
    (cleaner/register
     (host-key/new host (utils/encode-base64 key))))
@@ -20,43 +30,61 @@
     (host-key/new marker host type (utils/encode-base64 key) comment))))
 
 (defn get-host
+  "Returns the hostname for the host-key."
   [host-key]
   (host-key/get-host
    (cleaner/split-key host-key)))
 
 (defn get-type
+  "Returns the key type string for the host-key.
+  `\"ssh-rsa\"`, `\"ssh-dss\"`, `\"ecdsa-sha2-nistp256\"` or
+  `\"ssh-ed25519\"`.
+  "
   [host-key]
   (host-key/get-type
    (cleaner/split-key host-key)))
 
 (defn get-key
+  "Returns the key as a base64 encoded string."
   [host-key]
   (host-key/get-key
    (cleaner/split-key host-key)))
 
 (defn get-finger-print
+  "Returns the fingerprint of the key."
   [host-key agent]
   (host-key/get-finger-print
    (cleaner/split-key host-key)
-   (cleaner/split-key agent)
-   ))
+   (cleaner/split-key agent)))
 
 (defn get-comment
+  "Returns the comment associated with the key."
   [host-key]
   (host-key/get-comment
    (cleaner/split-key host-key)))
 
 (defn get-marker
+  "Returns any @ marker associated with the key. If no marker is
+  associated returns the empty string."
   [host-key]
   (host-key/get-marker
    (cleaner/split-key host-key)))
 
-(defn get-info [host-key agent]
+(defn get-info
+  "Returns all the associated information of the key as a single hashmap
+  with keys `:host`, `:type`, `:key`, `:finger-print`, `:comment` and
+  `:marker`."
+  [host-key agent]
   (host-key/get-info
    (cleaner/split-key host-key)
    (cleaner/split-key agent)))
 
-(defn get-infos [host-keys agent]
+(defn get-infos
+  "Given a sequence of host-key references and the bbssh agent
+  reference, return a hashmap of all the info for all the keys. The
+  keys are the host-key references and the values are as would be
+  returned from `get-info`."
+  [host-keys agent]
   (->> (host-key/get-infos
         (mapv cleaner/split-key host-keys)
         (cleaner/split-key agent))

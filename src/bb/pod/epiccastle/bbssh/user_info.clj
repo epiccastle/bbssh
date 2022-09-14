7cf0fe0a417f5a231d5f3816ab5d5ff1a91e0b20
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-14 15:43:38 +0800

    user info docs

--- src/bb/pod/epiccastle/bbssh/user_info.clj
+++ src/bb/pod/epiccastle/bbssh/user_info.clj
@@ -9,30 +9,41 @@
 
   ```clojure
   :get-password (fn [] ...)
-  ;; return the password for the user
-
-  :get-passphrase (fn [] ...)
-  ;; return the passphrase for the user
-
-  :prompt-yes-no (fn [^String question] ...)
-  ;; ask the user a yes/no question. Block until the answer is
-  ;; gathered. Return a truthy value if yes.
-
-  :prompt-passphrase (fn [^String question] ...)
-  ;; Ask the user for their passphrase. Block until it is gathered.
-  ;; Return true to continue connecting or false to quit the
-  ;; connection. Note: do not return the passphrase. That is gathered
-  ;; with a subsequent :get-passphrase if this function returned true.
-
-  :prompt-password (fn [^String question] ...)
-  ;; Ask the user for their password. Block until it is gathered.
-  ;; Return true to continue connecting or false to quit the
-  ;; connection. Note: do not return the password. That is gathered
-  ;; with a subsequent :get-password if this function returned true.
-
-  :show-message (fn [^String message] ...)
-  ;; Display for the user the specified message.
   ```
+    return the password for the user
+
+  ```clojure
+  :get-passphrase (fn [] ...)
+  ```
+    return the passphrase for the user
+
+  ```clojure
+  :prompt-yes-no (fn [^String question] ...)
+  ```
+    ask the user a yes/no question. Block until the answer is
+    gathered. Return a truthy value if yes.
+
+  ```clojure
+  :prompt-passphrase (fn [^String question] ...)
+  ```
+    Ask the user for their passphrase. Block until it is gathered.
+    Return true to continue connecting or false to quit the
+    connection. Note: do not return the passphrase. That is gathered
+    with a subsequent :get-passphrase if this function returned true.
+
+  ```clojure
+  :prompt-password (fn [^String question] ...)
+  ```
+    Ask the user for their password. Block until it is gathered.
+    Return true to continue connecting or false to quit the
+    connection. Note: do not return the password. That is gathered
+    with a subsequent :get-password if this function returned true.
+
+  ```clojure
+  :show-message (fn [^String message] ...)
+  ```
+    Display for the user the specified message.
+
   "
   [callbacks]
   (utils/new-invoker

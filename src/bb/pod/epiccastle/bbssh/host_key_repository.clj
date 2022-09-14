9de283d45bc3d48403450884060343d14a0001c4
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-14 14:47:08 +0800

    host-key-repository docstring for new started

--- src/bb/pod/epiccastle/bbssh/host_key_repository.clj
+++ src/bb/pod/epiccastle/bbssh/host_key_repository.clj
@@ -30,7 +30,49 @@
 
     result))
 
-(defn new [callbacks]
+(defn new
+  "Create a new host-key-repository. Pass in a hashmap containing
+  callbacks to be used for the methods of the repository. The hashmap
+  keys are optional. If a value is not specified a default is used.
+  The keys are as follows:
+
+  ```clojure
+  :check (fn [^String host ^bytes public-key] ...)
+  ```
+
+    Check the repository for the presence of the passed in public
+    key under the specified hostname. Hosts should be able to
+    have multiple keys of different key types. Thus you should
+    examine the key type and check that hosts keys for the same
+    type. Your function should return `:ok` if the key is present
+    and matches, `:not-included` if no matching key is found and
+    `:changed` if there is a conflicting key of that type stored
+    against the hostname.
+
+  ```clojure
+  :add (fn [^Keyword host-key ^Keyword user-info] ...)
+  ```
+
+    Add the referenced `host-key` into the repository. If the key
+    requires user interaction use the passed in `user-info` to
+    do so.
+
+  ```clojure
+  :remove (fn
+            ([^String host ^String type] ..)
+            ([^String host ^String type ^bytes public-key] ..)
+  ```
+
+    Remove the referenced `public-key` of `type` being stored
+    for `host`. If `public-key` is not passed, remove all keys
+    of `type` for `host`. `type` will be one of SSH public key
+    prefix strings `\"ssh-rsa\"`, `\"ssh-dss\"`,
+    `\"ecdsa-sha2-nistp256\"` or `\"ssh-ed25519\"`. Other key
+    types may be added with later releases and so should be
+    accomodated.
+
+  "
+  [callbacks]
   (utils/new-invoker
    {:call-sym 'pod.epiccastle.bbssh.impl.host-key-repository/new
     :args []

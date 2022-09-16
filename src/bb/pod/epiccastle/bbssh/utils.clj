9790757e208c660a087dba6bea7cbf0cc8732a52
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-16 12:13:50 +0800

    optional decoders

--- src/bb/pod/epiccastle/bbssh/utils.clj
+++ src/bb/pod/epiccastle/bbssh/utils.clj
@@ -12,6 +12,23 @@
 (defn encode-base64 [array]
   (.encodeToString encoder array))
 
+(defn opt-decode-base64
+  "If passed a string, decode it as base64 and return a byte array.
+  If passed a byte array, just return it as is."
+  [key]
+  (if (string? key)
+    (decode-base64 key)
+    key))
+
+(defn opt-get-bytes
+  "If passed a string, return the underlying byte array using the
+  encoding (defaults to utf8). If passed a byte arrayu, just return it
+  as it is."
+  [data & [encoding]]
+  (if (string? data)
+    (.getBytes data (or encoding "utf-8"))
+    data))
+
 (defn new-invoker [{:keys [call-sym args
                            callbacks
                            preprocess-args-fn

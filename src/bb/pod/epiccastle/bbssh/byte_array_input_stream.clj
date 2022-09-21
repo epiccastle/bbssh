e985b155d7ef50e8761e19d39f12f56920ca4415
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-21 11:16:51 +0800

    new byte-array-input-stream namespace

---
+++ src/bb/pod/epiccastle/bbssh/byte_array_input_stream.clj
@@ -0,0 +1,73 @@
+(ns pod.epiccastle.bbssh.byte-array-input-stream
+  (:refer-clojure :exclude [flush])
+  (:require [pod.epiccastle.bbssh.impl.byte-array-input-stream :as byte-array-input-stream]
+            [pod.epiccastle.bbssh.cleaner :as cleaner]
+            [pod.epiccastle.bbssh.utils :as utils]))
+
+(defn new
+  [string-or-bytes & [encoding]]
+  (cleaner/register
+   (if (string? string-or-bytes)
+     (byte-array-input-stream/new-from-string
+      string-or-bytes encoding)
+     (byte-array-input-stream/new-from-bytes
+      (utils/encode-base64 string-or-bytes)))))
+
+(defn available
+  "Returns the number of remaining bytes that can be read (or skipped over) from this input stream."
+  [stream]
+  (byte-array-input-stream/available
+   (cleaner/split-key stream)))
+
+(defn close
+  "Closing a ByteArrayInputStream has no effect."
+  [stream]
+  (byte-array-input-stream/close
+   (cleaner/split-key stream)))
+
+(defn mark
+  "Set the current marked position in the stream."
+  [stream read-ahead-limit]
+  (byte-array-input-stream/mark
+   (cleaner/split-key stream)
+   read-ahead-limit))
+
+(defn mark-supported
+  "Tests if this InputStream supports mark/reset."
+  [stream]
+  (byte-array-input-stream/mark-supported
+   (cleaner/split-key stream)))
+
+(defn read
+  "`(read stream)`
+  Read a single byte from the stream. Returns an int. Blocks
+  if a byte is not available.
+
+  `(read stream byte-array offset length)`
+  Try and read `length` bytes from the `stream` and store them into
+  a `byte-array` starting at `offset`. Returns the number of bytes
+  successfully read. Does not block.
+  "
+  ([stream]
+   (byte-array-input-stream/read
+    (cleaner/split-key stream)))
+  ([stream bytes offset length]
+   (let [[bytes-read base64] (byte-array-input-stream/read
+                              (cleaner/split-key stream)
+                              length)]
+     (when (pos? bytes-read)
+       (System/arraycopy (utils/decode-base64 base64) 0 bytes offset bytes-read))
+     bytes-read)))
+
+(defn reset
+  "Resets the buffer to the marked position."
+  [stream]
+  (byte-array-input-stream/reset
+   (cleaner/split-key stream)))
+
+(defn skip
+  "Skips `n` bytes of input from this input stream."
+  [stream n]
+  (byte-array-input-stream/skip
+   (cleaner/split-key stream)
+   n))
---
+++ src/clj/pod/epiccastle/bbssh/impl/byte_array_input_stream.clj
@@ -0,0 +1,69 @@
+(ns pod.epiccastle.bbssh.impl.byte-array-input-stream
+  (:refer-clojure :exclude [read])
+  (:require [bbssh.impl.references :as references]
+            [pod.epiccastle.bbssh.impl.callbacks :as callbacks]
+            [pod.epiccastle.bbssh.impl.cleaner :as cleaner]
+            [bbssh.impl.utils :as utils])
+  (:import [java.io
+            PipedInputStream PipedOutputStream
+            ByteArrayInputStream ByteArrayOutputStream
+            InputStream]
+           [java.util Arrays]))
+
+;; pod.epiccastle.bbssh.impl.* are invoked on pod side.
+
+(set! *warn-on-reflection* true)
+
+(defn new-from-string [^String string & [^String encoding]]
+  (references/add-instance
+   (ByteArrayInputStream.
+    ^bytes (.getBytes string (or encoding "utf-8")))))
+
+(defn new-from-bytes [^String string]
+  (references/add-instance
+   (ByteArrayInputStream.
+    ^bytes (utils/decode-base64 string))))
+
+(defn available [stream]
+  (.available
+   ^ByteArrayInputStream (references/get-instance stream)))
+
+(defn close [stream]
+  (.close
+   ^ByteArrayInputStream (references/get-instance stream)))
+
+(defn mark [stream read-ahead-limit]
+  (.mark
+   ^ByteArrayInputStream (references/get-instance stream)
+   read-ahead-limit))
+
+(defn mark-supported [stream]
+  (.markSupported
+   ^ByteArrayInputStream (references/get-instance stream)))
+
+(defn ^:blocking read
+  ([stream]
+   (.read
+    ^ByteArrayInputStream (references/get-instance stream)))
+  ([stream bytes]
+   (let [arr (byte-array bytes)
+         bytes-read
+         (.read
+          ^ByteArrayInputStream (references/get-instance stream)
+          arr
+          0
+          bytes)]
+     [bytes-read
+      (case bytes-read
+        -1 nil
+        0 ""
+        (utils/encode-base64 (Arrays/copyOfRange arr 0 bytes-read)))])))
+
+(defn reset [stream]
+  (.reset
+   ^ByteArrayInputStream (references/get-instance stream)))
+
+(defn skip [stream n]
+  (.skip
+   ^ByteArrayInputStream (references/get-instance stream)
+   n))

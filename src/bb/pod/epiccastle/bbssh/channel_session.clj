10245f4644ca78eb3c9998148182f949942ceeba
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-26 11:18:54 +0800

    remove channel session

--- src/bb/pod/epiccastle/bbssh/channel_session.clj
+++
@@ -1,3 +0,0 @@
-(ns pod.epiccastle.bbssh.channel-session
-  (:require [pod.epiccastle.bbssh.pod.channel-exec :as channel-session]
-            [pod.epiccastle.bbssh.cleaner :as cleaner]))
--- src/clj/pod/epiccastle/bbssh/pod/channel_session.clj
+++
@@ -1,9 +0,0 @@
-(ns pod.epiccastle.bbssh.pod.channel-session
-  (:require [bbssh.impl.references :as references]
-            [bbssh.impl.utils :as utils])
-  (:import [com.jcraft.jsch JSch Session UserInfo ChannelSession])
-  )
-
-;; pod.epiccastle.bbssh.pod.* are invoked on pod side.
-
-(set! *warn-on-reflection* true)

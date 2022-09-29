b7c1fe8899f3ae3db0936f699d751ca43345c118
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-29 14:11:18 +0800

    new BbsshUtils test code

--- src/c/BbsshUtils.java
+++ src/c/BbsshUtils.java
@@ -1,18 +1,75 @@
-public class BbsshUtils {
-    // terminal tty
-    public static native int is_a_tty();
+import static java.util.Objects.requireNonNull;
+import static java.lang.System.getProperty;
+import static java.lang.System.getenv;
+import static java.lang.Thread.currentThread;
+import static java.util.Locale.ENGLISH;
 
-    // terminal size queries
-    public static native int get_terminal_width();
-    public static native int get_terminal_height();
+//import com.oracle.svm.core.SubstrateOptions;
+//import com.oracle.svm.core.option.OptionUtils;
+//import com.oracle.svm.core.c.ProjectHeaderFile;
+//import com.oracle.svm.core.c.CGlobalData;
+//import com.oracle.svm.core.c.CGlobalDataFactory;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.UnmanagedMemory;
+import org.graalvm.nativeimage.PinnedObject;
+import org.graalvm.nativeimage.StackValue;
+import org.graalvm.nativeimage.c.CContext;
+import org.graalvm.nativeimage.c.constant.CConstant;
+import org.graalvm.nativeimage.c.constant.CEnum;
+import org.graalvm.nativeimage.c.constant.CEnumLookup;
+import org.graalvm.nativeimage.c.constant.CEnumValue;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.function.CEntryPointLiteral;
+import org.graalvm.nativeimage.c.function.CFunction;
+import org.graalvm.nativeimage.c.function.CFunctionPointer;
+import org.graalvm.nativeimage.c.function.InvokeCFunctionPointer;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CCharPointerPointer;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.CLongPointer;
+import org.graalvm.nativeimage.c.type.WordPointer;
+import org.graalvm.nativeimage.c.type.VoidPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.nativeimage.c.struct.CField;
+import org.graalvm.nativeimage.c.struct.CFieldAddress;
+import org.graalvm.nativeimage.c.struct.CStruct;
+import org.graalvm.nativeimage.c.struct.CPointerTo;
+import org.graalvm.nativeimage.c.struct.SizeOf;
+import org.graalvm.word.Pointer;
+import org.graalvm.word.PointerBase;
 
-    // terminal raw mode
-    public static native void enter_raw_mode(int quiet);
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@CContext(BbsshUtils.Directives.class)
+public final class BbsshUtils {
+
+    public static final class Directives implements CContext.Directives {
+        @Override
+        public List<String> getHeaderFiles() {
+            return Collections.singletonList("\"BbsshUtils.h\"");
+        }
+
+        @Override
+        public List<String> getLibraries() {
+            return Arrays.asList("bbssh");
+        }
+    }
+
+    @CFunction("leave_raw_mode")
     public static native void leave_raw_mode(int quiet);
 
-    // unix domain socket access
-    public static native int ssh_open_auth_socket(String path);
-    public static native void ssh_close_auth_socket(int socket);
-    public static native int ssh_auth_socket_read(int fd, byte[] buf, int count);
-    public static native int ssh_auth_socket_write(int fd, byte[] buf, int count);
+    @CFunction("enter_raw_mode")
+    public static native void enter_raw_mode(int quiet);
+
+
 }

db3a6b8a74dbbcf230cda1bed5323c1c3fa507aa
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-29 14:11:35 +0800

    limit what we call to the new subset

--- src/clj/pod/epiccastle/bbssh/pod/socket.clj
+++ src/clj/pod/epiccastle/bbssh/pod/socket.clj
@@ -3,16 +3,19 @@
   (:require [bbssh.impl.utils :as utils]))
 
 (defn open [sock-path]
-  (BbsshUtils/ssh-open-auth-socket sock-path))
+  ;;(BbsshUtils/ssh-open-auth-socket sock-path)
+  )
 
 (defn close [sock-fd]
-  (BbsshUtils/ssh-close-auth-socket sock-fd))
+  ;;(BbsshUtils/ssh-close-auth-socket sock-fd)
+  )
 
 (defn write [sock-fd base64]
   (let [buffer (utils/decode-base64 base64)]
-    (BbsshUtils/ssh-auth-socket-write sock-fd buffer (count buffer))))
+    ;;(BbsshUtils/ssh-auth-socket-write sock-fd buffer (count buffer))
+    ))
 
 (defn read [sock-fd size]
   (let [buffer (byte-array size)]
-    (BbsshUtils/ssh-auth-socket-read sock-fd buffer size)
+    ;;(BbsshUtils/ssh-auth-socket-read sock-fd buffer size)
     (utils/encode-base64 buffer)))
--- src/clj/pod/epiccastle/bbssh/pod/terminal.clj
+++ src/clj/pod/epiccastle/bbssh/pod/terminal.clj
@@ -1,13 +1,17 @@
 (ns pod.epiccastle.bbssh.pod.terminal)
 
 (defn is-terminal? []
-  (pos? (BbsshUtils/is-a-tty)))
+  ;;(pos? (BbsshUtils/is-a-tty))
+  )
 
 (defn get-width []
-  (BbsshUtils/get-terminal-width))
+  ;;(BbsshUtils/get-terminal-width)
+  )
 
 (defn enter-raw-mode [n]
-  (BbsshUtils/enter-raw-mode n))
+  (BbsshUtils/enter-raw-mode n)
+  )
 
 (defn leave-raw-mode [n]
-  (BbsshUtils/leave-raw-mode n))
+  (BbsshUtils/leave-raw-mode n)
+  )

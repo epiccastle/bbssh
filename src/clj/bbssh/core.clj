556aafa53bb78702af75e02a52cfbcd0511534b0
Crispin Wellington <retrogradeorbit@gmail.com>
2022-09-30 15:43:35 +0800

    remove lib calls

--- src/clj/bbssh/core.clj
+++ src/clj/bbssh/core.clj
@@ -17,26 +17,7 @@
        (= "runtime" (System/getProperty "org.graalvm.nativeimage.imagecode"))))
 
 (defn -main [& args]
-  #_(lib/init!)
-  (prn 'load (clojure.lang.RT/loadLibrary "bbssh"))
-  #_(System/load "/home/crispin/dev/clojure/bbssh/libbbssh.so")
-  #_(prn (System/loadLibrary "bbssh"))
-  (prn
-   (BbsshUtils/is-stdout-a-tty)
-   (BbsshUtils/get-terminal-width)
-   (BbsshUtils/get-terminal-height)
-   )
-  #_(prn
-   (BbsshUtils/ssh-open-auth-socket
-    (.get (org.graalvm.nativeimage.c.type.CTypeConversion/toCString
-           "/run/user/1000/keyring/ssh")))
-   )
-  (prn (BbsshUtils/enter-raw-mode 1))
-  (let [result (read-line)]
-    (prn (BbsshUtils/leave-raw-mode 1))
-    (prn result)
-    )
-  #_(let [{:keys [options]} (cli/parse-opts args [["-v" "--version"]])]
+  (let [{:keys [options]} (cli/parse-opts args [["-v" "--version"]])]
     (if (:version options)
       (println "bbssh version" version)
       (do
@@ -45,7 +26,6 @@
             (println "Error: bbssh needs to be run as a babashka pod."))
           (System/exit 1))
 
-        ;;(lib/init!)
         (when-not (native-image?)
           (clojure.lang.RT/loadLibrary "bbssh"))
         (pod/main)))))

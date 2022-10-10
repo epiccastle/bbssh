92c1b22c3f5da61792b83d663383688ab323dd03
Crispin Wellington <retrogradeorbit@gmail.com>
2022-10-10 14:35:59 +0800

    test setting port forwarding in core/ssh call

--- test/bb_test/test_port_forward.clj
+++ test/bb_test/test_port_forward.clj
@@ -84,3 +84,67 @@
             (.contains "Connection refused"))))
 
   (docker/cleanup))
+
+(deftest test-port-forward-local-core-ssh
+  (docker/cleanup)
+  (docker/build {:root-password "root-access-please"})
+  (docker/start {:ssh-port 9876})
+
+  (let [session
+        (bbssh/ssh
+         "localhost"
+         {:port 9876
+          :username "root"
+          :password "root-access-please"
+          :strict-host-key-checking false
+          :port-forward-local [{:local-port 56780
+                                :remote-host "localhost"
+                                :remote-port 22}]
+          })]
+    (is (= (session/get-port-forwarding-local session)
+           [{:local-port 56780
+             :remote-host "localhost"
+             :remote-port 22}]))
+    (let [sock (Socket. "localhost" 56780)
+          output (.getOutputStream sock)
+          input (.getInputStream sock)]
+      (is (= (read-stream-line input)
+             "SSH-2.0-OpenSSH_9.0\r\n"))
+      (.write output (.getBytes "invalid\n"))
+      (is (= (read-stream-line input)
+             "Invalid SSH identification string.\r\n"))
+      (.close sock))
+    (session/delete-port-forwarding-local session {:local-port 56780})
+    (is (empty? (session/get-port-forwarding-local session)))
+    (is (thrown? java.net.ConnectException (Socket. "localhost" 56780))))
+
+  (docker/cleanup))
+
+(deftest test-port-forward-remote-core-ssh
+  (docker/cleanup)
+  (docker/build {:root-password "root-access-please"})
+  (docker/start {:ssh-port 9876})
+
+  (let [session
+        (bbssh/ssh
+         "localhost"
+         {:port 9876
+          :username "root"
+          :password "root-access-please"
+          :strict-host-key-checking false
+          :port-forward-remote [{:remote-port 56780
+                                 :local-host "epiccastle.io"
+                                 :local-port 80}]})]
+    (is (= (session/get-port-forwarding-remote session)
+           [{:remote-port 56780
+             :local-host "epiccastle.io"
+             :local-port 80}]))
+    (is (-> (docker/exec "curl http://localhost:56780")
+            (.contains "301 Moved Permanently")))
+    (session/delete-port-forwarding-remote session {:remote-port 56780})
+    (is (empty? (session/get-port-forwarding-remote session)))
+    (is (-> (docker/exec! "curl http://localhost:56780")
+            :err
+            (.contains "Connection refused"))))
+
+  (docker/cleanup))
